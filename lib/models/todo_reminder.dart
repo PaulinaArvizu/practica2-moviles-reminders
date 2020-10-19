8bed3cec6a445914cd2bcdf3a60c34f0a8b91614
PaulinaArvizu <is707571@iteso.mx>
2020-10-19 15:14:58 -0500

    convertir en adapter de Hive y utilizar build runner para generar el adapter

--- lib/models/todo_reminder.dart
+++ lib/models/todo_reminder.dart
@@ -1,6 +1,13 @@
-// TODO: convertir en adapter de Hive y utilizar build runner para generar el adapter
+// DONE: convertir en adapter de Hive y utilizar build runner para generar el adapter
+
+import 'package:hive/hive.dart';
+part 'todo_reminder.g.dart';
+
+@HiveType(typeId: 1, adapterName: "ReminderAdapter")
 class TodoRemainder {
+  @HiveField(0)
   final String todoDescription;
+  @HiveField(1)
   final String hour;
 
   TodoRemainder({
